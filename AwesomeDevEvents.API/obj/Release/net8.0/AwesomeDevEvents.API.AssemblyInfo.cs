adc1a99ff1bd769b8dc599f5cc1baaf117bb56a9
Patrick <patrickrebecchi@hotmail.com>
2024-06-18 20:48:24 -0300

    - Criada a pasta Mappings e o arquivo MappingProfile.cs para configuração do AutoMapper. - Criadas as classes Financeiro.cs, Pessoa.cs, Relatorio.cs e Servico.cs. - Atualiçoes das classes EnderecoDTO, FinanceiroDTO, PessoaDTO, RelatorioDTO e ServicoDTO. - Configuradas validações usando DataAnnotations nas classes DTO. - Atualizados os controladores correspondentes para utilizar as novas DTOs."

--- AwesomeDevEvents.API/obj/Release/net8.0/AwesomeDevEvents.API.AssemblyInfo.cs
+++ AwesomeDevEvents.API/obj/Release/net8.0/AwesomeDevEvents.API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AwesomeDevEvents.API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bae85e345fca3426f5fa17d06bc305cdf749c44")]
 [assembly: System.Reflection.AssemblyProductAttribute("AwesomeDevEvents.API")]
